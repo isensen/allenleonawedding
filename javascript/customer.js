a5996388d2d51524cb71a4c13dedeeaf4141c38a
allenzerg001 <allenzerg001@gmail.com>
2012-03-12 21:47:27 +0800

    add invite.html and css

--- javascript/customer.js
+++ javascript/customer.js
@@ -10,23 +10,12 @@ function getQueryParameter(qs)
     return decodeURIComponent(re);
 }
 
-var customers =["miaochenjing","chenyuanyuan","chenzhongyi","daijiahui","duke","hanjiayan","huanglei","jinliping","jinsong","liangxiao","raohaiyan","shengtao","shenhuangyu","shiyif","tangqinyan","wangjie","wangqianfeng","weijiongnan","wuchao","wuyichen","xujie","yangyifang","yelingwei","yezhengyan","yinlu","yudan","yuqing","yuxiaobo","zhangheng","zhangting","zhangzhang","zhouchenhuan","zhoulijun","zhouzhi"];
 function getCustomer(){
     var name = getQueryParameter("u");
     if(name==undefined || name.length==0){
         name = "nin";
     }
-    var find = false;
-    for(var i=0;i<customers.length;i++){
-        if(customers[i]==name){
-            find = true;
-            break;
-        }
-    }
-    if(!find){
-        name = "nin";
-    }
-
     name = "images/custom/"+name+".png";
     return name;
 }
+
